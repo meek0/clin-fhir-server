c1e5b2aee391bbe151d029393f9d676d3f20b595
YanLabCHU <ymoussaoui@ferlab.bio>
2020-10-01 10:09:05 -0400

    log successful message in UpdateResponseListener.java

--- src/main/java/bio/ferlab/clin/es/data/UpdateResponseListener.java
+++ src/main/java/bio/ferlab/clin/es/data/UpdateResponseListener.java
@@ -1,15 +1,9 @@
 package bio.ferlab.clin.es.data;
 
-import bio.ferlab.clin.exceptions.FailedToUpdateIndexException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.io.IOUtils;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.ResponseListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.nio.charset.Charset;
 
 public class UpdateResponseListener implements ResponseListener {
     private static final Logger logger = LoggerFactory.getLogger(UpdateResponseListener.class);
@@ -21,16 +15,7 @@ public class UpdateResponseListener implements ResponseListener {
 
     @Override
     public void onSuccess(Response response) {
-        try {
-            final String content = IOUtils.toString(response.getEntity().getContent(), Charset.defaultCharset());
-            final ElasticResponse.Index parsed = new ObjectMapper().readValue(content, ElasticResponse.Index.class);
-            if (parsed.getShards().getSuccessful() != 1 || parsed.getShards().getFailed() > 0) {
-                throw new FailedToUpdateIndexException(this.index);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new FailedToUpdateIndexException(this.index);
-        }
+        logger.info(String.format("Index [%s] successfully updated.", this.index));
     }
 
     @Override
