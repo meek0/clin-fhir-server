d7876fef983ee1b5fada0707c7f8a252e111060d
YanLabCHU <ymoussaoui@ferlab.bio>
2020-11-04 09:48:35 -0500

    Updates RestClient to index/delete non-fhir data

--- src/main/java/bio/ferlab/clin/interceptors/IndexerInterceptor.java
+++ src/main/java/bio/ferlab/clin/interceptors/IndexerInterceptor.java
@@ -1,6 +1,10 @@
 package bio.ferlab.clin.interceptors;
 
 import bio.ferlab.clin.es.ElasticsearchRestClient;
+import bio.ferlab.clin.es.ElasticsearchRestClient.IndexData;
+import bio.ferlab.clin.es.data.PatientData;
+import bio.ferlab.clin.es.data.builder.PatientDataBuilder;
+import bio.ferlab.clin.utils.JsonGenerator;
 import ca.uhn.fhir.interceptor.api.Hook;
 import ca.uhn.fhir.interceptor.api.Interceptor;
 import ca.uhn.fhir.interceptor.api.Pointcut;
@@ -8,7 +12,6 @@ import ca.uhn.fhir.jpa.starter.HapiProperties;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.Resource;
 import org.springframework.context.ApplicationContext;
 
 @Interceptor
@@ -16,30 +19,39 @@ public class IndexerInterceptor {
     private static final String INDEX_PATIENT = HapiProperties.getBioEsIndexPatient();
 
     private final ElasticsearchRestClient client;
+    private final PatientDataBuilder patientDataBuilder;
+    private final JsonGenerator parser;
 
     public IndexerInterceptor(ApplicationContext appContext) {
-        // Autowired doesn't work, so we need the ApplicationContext to get the bean.
         this.client = appContext.getBean(ElasticsearchRestClient.class);
+        this.patientDataBuilder = appContext.getBean(PatientDataBuilder.class);
+        this.parser = appContext.getBean(JsonGenerator.class);
     }
 
     @Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_CREATED)
-    public void resourceCreated(IBaseResource resource) {
-        if (resource instanceof Patient) {
-            client.index(INDEX_PATIENT, (Resource) resource);
-        }
+    public void resourceCreated(IBaseResource resource, RequestDetails requestDetails) {
+        this.indexToEs(resource, requestDetails);
     }
 
     @Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_UPDATED)
-    public void resourceUpdated(IBaseResource oldResource, IBaseResource newResource) {
-        if (newResource instanceof Patient) {
-            client.index(INDEX_PATIENT, (Resource) newResource);
-        }
+    public void resourceUpdated(IBaseResource oldResource, IBaseResource newResource, RequestDetails requestDetails) {
+        this.indexToEs(newResource, requestDetails);
     }
 
     @Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_DELETED)
     public void resourceDeleted(IBaseResource resource, RequestDetails requestDetails) {
         if (resource instanceof Patient) {
-            client.delete(INDEX_PATIENT, (Resource) resource);
+            client.delete(INDEX_PATIENT, resource.getIdElement().getIdPart());
+        }
+    }
+
+    private void indexToEs(IBaseResource resource, RequestDetails requestDetails) {
+        if (resource instanceof Patient) {
+            final PatientData patientData = this.patientDataBuilder.fromJson(requestDetails.getRequestContentsIfLoaded());
+            if (patientData != null) {
+                final IndexData data = new IndexData(resource.getIdElement().getIdPart(), parser.toString(patientData));
+                client.index(INDEX_PATIENT, data);
+            }
         }
     }
 }
