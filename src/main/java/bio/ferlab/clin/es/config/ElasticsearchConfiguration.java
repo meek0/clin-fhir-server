e049c5a4c3226f8a10133d02d321fa32fa0a61f9
YanLabCHU <ymoussaoui@ferlab.bio>
2020-11-23 08:55:42 -0500

    Setup custom client callback to ES client

--- src/main/java/bio/ferlab/clin/es/config/ElasticsearchConfiguration.java
+++ src/main/java/bio/ferlab/clin/es/config/ElasticsearchConfiguration.java
@@ -4,6 +4,7 @@ import bio.ferlab.clin.es.ElasticsearchRestClient;
 import bio.ferlab.clin.es.data.ElasticsearchData;
 import ca.uhn.fhir.jpa.starter.HapiProperties;
 import org.apache.http.HttpHost;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,12 +19,17 @@ public class ElasticsearchConfiguration {
     public ElasticsearchData esData() {
         RestClient client = RestClient.builder(
                 new HttpHost(this.host, this.port, this.scheme)
-        ).build();
+        )
+                .setMaxRetryTimeoutMillis(1000 * 60)
+                .setHttpClientConfigCallback(httpClientBuilder -> HttpAsyncClientBuilder.create().setKeepAliveStrategy(
+                        (response, context) -> 1000 * 60)
+                )
+                .build();
         return new ElasticsearchData(client, this.host);
     }
 
     @Bean
-    public ElasticsearchRestClient esRestClient(ElasticsearchData esData){
+    public ElasticsearchRestClient esRestClient(ElasticsearchData esData) {
         return new ElasticsearchRestClient(esData);
     }
 }
