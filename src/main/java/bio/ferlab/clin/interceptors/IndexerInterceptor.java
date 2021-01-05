423a0474323830dd0ed5da2da953f4e26f558a80
YanLabCHU <ymoussaoui@ferlab.bio>
2021-01-05 11:40:15 -0500

    feat: #606 Enable indexation on ServiceRequest update

--- src/main/java/bio/ferlab/clin/interceptors/IndexerInterceptor.java
+++ src/main/java/bio/ferlab/clin/interceptors/IndexerInterceptor.java
@@ -3,7 +3,9 @@ package bio.ferlab.clin.interceptors;
 import bio.ferlab.clin.es.ElasticsearchRestClient;
 import bio.ferlab.clin.es.ElasticsearchRestClient.IndexData;
 import bio.ferlab.clin.es.data.PatientData;
+import bio.ferlab.clin.es.data.UpdateRequestData;
 import bio.ferlab.clin.es.data.builder.PatientDataBuilder;
+import bio.ferlab.clin.utils.Extensions;
 import bio.ferlab.clin.utils.JsonGenerator;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.interceptor.api.Hook;
@@ -15,8 +17,10 @@ import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.api.server.ResponseDetails;
 import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.BooleanType;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.ServiceRequest;
 import org.springframework.context.ApplicationContext;
 
 import java.util.List;
@@ -42,6 +46,21 @@ public class IndexerInterceptor {
         this.parser = appContext.getBean(FhirContext.class).newJsonParser();
     }
 
+    @Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_UPDATED)
+    public void resourceUpdated(IBaseResource oldResource, IBaseResource newResource, RequestDetails requestDetails) {
+        if (!isEsBundled(requestDetails) && newResource instanceof ServiceRequest) {
+            final ServiceRequest serviceRequest = (ServiceRequest) newResource;
+            if (serviceRequest.hasSubject()) {
+                final BooleanType isSubmitted = (BooleanType) serviceRequest.getExtensionByUrl(Extensions.IS_SUBMITTED).getValue();
+                final UpdateRequestData updateData = new UpdateRequestData(serviceRequest.getStatus().toCode(), isSubmitted.getValue());
+                final IndexData indexData = new IndexData(serviceRequest.getSubject().getReference().split("/")[1], updateData.stringify(jsonGenerator));
+                client.update(INDEX_PATIENT, indexData);
+            } else {
+                throw new ca.uhn.fhir.rest.server.exceptions.PreconditionFailedException("Service Request without subject");
+            }
+        }
+    }
+
     @Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_DELETED)
     public void resourceDeleted(IBaseResource resource) {
         if (resource instanceof Patient) {
@@ -56,8 +75,7 @@ public class IndexerInterceptor {
             IBaseResource resource,
             ResponseDetails responseDetails) {
 
-        if (requestDetails.getParameters().containsKey("id") &&
-                requestDetails.getParameters().get("id")[0].contentEquals(HapiProperties.getBioEsBundleId())) {
+        if (isEsBundled(requestDetails)) {
 
             final String content = new String(requestDetails.getRequestContentsIfLoaded());
             final Bundle bundle = parser.parseResource(Bundle.class, content);
@@ -70,6 +88,11 @@ public class IndexerInterceptor {
             indexToEs(bundle);
         }
         return true;
+    }
+
+    private boolean isEsBundled(RequestDetails requestDetails) {
+        return requestDetails.getParameters().containsKey("id") &&
+                requestDetails.getParameters().get("id")[0].contentEquals(HapiProperties.getBioEsBundleId());
     }
 
 
