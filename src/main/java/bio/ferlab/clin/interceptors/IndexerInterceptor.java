0c70222c7eada9d46f5367529f7fd4158a7c203d
YanLabCHU <ymoussaoui@ferlab.bio>
2021-02-12 12:30:15 -0500

    feat: #663 Implements new elasticsearch indexation architecture

--- src/main/java/bio/ferlab/clin/interceptors/IndexerInterceptor.java
+++ src/main/java/bio/ferlab/clin/interceptors/IndexerInterceptor.java
@@ -2,11 +2,10 @@ package bio.ferlab.clin.interceptors;
 
 import bio.ferlab.clin.es.ElasticsearchRestClient;
 import bio.ferlab.clin.es.ElasticsearchRestClient.IndexData;
-import bio.ferlab.clin.es.PatientDataExtractor;
+import bio.ferlab.clin.es.IndexerHelper;
 import bio.ferlab.clin.es.data.PatientData;
-import bio.ferlab.clin.es.data.UpdateRequestData;
-import bio.ferlab.clin.es.data.builder.PatientDataBuilder;
-import bio.ferlab.clin.utils.Extensions;
+import bio.ferlab.clin.es.PatientIdExtractor;
+import bio.ferlab.clin.es.PatientDataBuilder;
 import bio.ferlab.clin.utils.JsonGenerator;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.interceptor.api.Hook;
@@ -14,15 +13,14 @@ import ca.uhn.fhir.interceptor.api.Interceptor;
 import ca.uhn.fhir.interceptor.api.Pointcut;
 import ca.uhn.fhir.jpa.starter.HapiProperties;
 import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.api.RequestTypeEnum;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
-import ca.uhn.fhir.rest.api.server.ResponseDetails;
-import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.*;
 import org.springframework.context.ApplicationContext;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Intercepts requests CREATE/UPDATE/DELETE requests, and index patient data to ES when needed.
@@ -33,31 +31,16 @@ import java.util.List;
 public class IndexerInterceptor {
     private static final String INDEX_PATIENT = HapiProperties.getBioEsIndexPatient();
 
-    private final PatientDataExtractor patientDataExtractor;
+    private final PatientIdExtractor patientIdExtractor;
+    private final PatientDataBuilder patientDataBuilder;
     private final ElasticsearchRestClient client;
     private final JsonGenerator jsonGenerator;
-    private final IParser parser;
 
     public IndexerInterceptor(ApplicationContext appContext) {
-        this.patientDataExtractor = appContext.getBean(PatientDataExtractor.class);
+        this.patientIdExtractor = appContext.getBean(PatientIdExtractor.class);
+        this.patientDataBuilder = appContext.getBean(PatientDataBuilder.class);
         this.client = appContext.getBean(ElasticsearchRestClient.class);
         this.jsonGenerator = appContext.getBean(JsonGenerator.class);
-        this.parser = appContext.getBean(FhirContext.class).newJsonParser();
-    }
-
-    @Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_UPDATED)
-    public void resourceUpdated(IBaseResource oldResource, IBaseResource newResource, RequestDetails requestDetails) {
-        if (!isEsBundled(requestDetails) && newResource instanceof ServiceRequest) {
-            final ServiceRequest serviceRequest = (ServiceRequest) newResource;
-            if (serviceRequest.hasSubject()) {
-                final BooleanType isSubmitted = (BooleanType) serviceRequest.getExtensionByUrl(Extensions.IS_SUBMITTED).getValue();
-                final UpdateRequestData updateData = new UpdateRequestData(serviceRequest.getStatus().toCode(), isSubmitted.getValue());
-                final IndexData indexData = new IndexData(serviceRequest.getSubject().getReference().split("/")[1], updateData.stringify(jsonGenerator));
-                client.update(INDEX_PATIENT, indexData);
-            } else {
-                throw new ca.uhn.fhir.rest.server.exceptions.PreconditionFailedException("Service Request without subject");
-            }
-        }
     }
 
     @Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_DELETED)
@@ -68,39 +51,22 @@ public class IndexerInterceptor {
     }
 
     @Hook(Pointcut.SERVER_OUTGOING_RESPONSE)
-    public boolean response(
-            RequestDetails requestDetails,
-            ServletRequestDetails servletRequestDetails,
-            IBaseResource resource,
-            ResponseDetails responseDetails) {
+    public boolean response(RequestDetails requestDetails) {
+        if (requestDetails.getRequestType() == RequestTypeEnum.POST || requestDetails.getRequestType() == RequestTypeEnum.PUT) {
+            final IBaseResource processedResource = requestDetails.getResource();
+            if (IndexerHelper.isIndexable(processedResource)) {
+                final Set<String> ids = this.patientIdExtractor.extract(processedResource);
+                if (ids != null) {
+                    final List<PatientData> patientDataList = this.patientDataBuilder.fromIds(ids);
+                    patientDataList.forEach(System.out::println);
 
-        if (isEsBundled(requestDetails)) {
-            final String content = new String(requestDetails.getRequestContentsIfLoaded());
-            final Bundle bundle = parser.parseResource(Bundle.class, content);
-            final Bundle responseBundle = (Bundle) responseDetails.getResponseResource();
-            final List<Bundle.BundleEntryComponent> entries = bundle.getEntry();
-            for (int i = 0; i < entries.size(); i++) {
-                final String id = responseBundle.getEntry().get(i).getResponse().getLocation().split("/")[1];
-                entries.get(i).getResource().setId(id);
+                    patientDataList.forEach(patientData -> {
+                        final IndexData data = new IndexData(patientData.getId(), jsonGenerator.toString(patientData));
+                        client.index(INDEX_PATIENT, data);
+                    });
+                }
             }
-            indexToEs(bundle);
         }
         return true;
-    }
-
-    private boolean isEsBundled(RequestDetails requestDetails) {
-        return requestDetails.getParameters().containsKey("id") &&
-                (requestDetails.getParameters().get("id")[0].contentEquals(HapiProperties.getBioEsPatientBundleId()) ||
-                        requestDetails.getParameters().get("id")[0].contentEquals(HapiProperties.getBioEsRequestBundleId()));
-    }
-
-
-    private void indexToEs(Bundle bundle) {
-        final List<PatientData> indexData = patientDataExtractor.extract(bundle);
-
-        indexData.forEach(patientData -> {
-            final IndexData data = new IndexData(patientData.getId(), jsonGenerator.toString(patientData));
-            client.index(INDEX_PATIENT, data);
-        });
     }
 }
