fc0e57ad71ba5ab2bf979d95f4aee8c0d9df76b3
YanLabCHU <ymoussaoui@ferlab.bio>
2021-07-08 14:52:43 -0400

    feat: Assume valid reference

--- src/main/java/bio/ferlab/clin/interceptors/ValidationInterceptor.java
+++ src/main/java/bio/ferlab/clin/interceptors/ValidationInterceptor.java
@@ -31,6 +31,7 @@ public class ValidationInterceptor extends RequestValidatingInterceptor {
         module.setAnyExtensionsAllowed(false);
         module.setErrorForUnknownProfiles(true);
         module.setNoTerminologyChecks(true);
+        module.setAssumeValidRestReferences(true);
         setValidatorModules(Collections.singletonList(module));
         return super.incomingRequestPostProcessed(requestDetails, request, response);
     }
