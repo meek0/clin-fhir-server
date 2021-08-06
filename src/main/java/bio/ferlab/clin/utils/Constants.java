2de3cd3ae5a53fd9fb4509960497c293159fc949
YanLabCHU <ymoussaoui@ferlab.bio>
2021-08-06 14:51:42 -0400

    feat: CLIN-182 Export op auth rules

--- src/main/java/bio/ferlab/clin/utils/Constants.java
+++ src/main/java/bio/ferlab/clin/utils/Constants.java
@@ -3,4 +3,5 @@ package bio.ferlab.clin.utils;
 public class Constants {
     public static final String REQUESTER_DATA_KEY = "requester-data";
     public static final String VALIDATE_OPERATION = "$validate";
+    public static final String EXPORT_OPERATION = "$export";
 }
