397e3a7f32157ee2cb5382e039b5a6cefd609c20
jecos <jcostanza@ferlab.bio>
2022-05-18 21:52:39 -0400

    fix: CLIN-1000 Remove ResponseTerminologyDisplayPopulationInterceptor because cause bundle failed

--- src/main/java/ca/uhn/fhir/jpa/app/BaseJpaRestfulServer.java
+++ src/main/java/ca/uhn/fhir/jpa/app/BaseJpaRestfulServer.java
@@ -431,6 +431,6 @@ public class BaseJpaRestfulServer extends RestfulServer {
         registerInterceptor(new DeleteConflictInterceptor());
         
         // Auto add Coding.display if Coding.system/code is defined in the FHIR model
-        registerInterceptor(new ResponseTerminologyDisplayPopulationInterceptor(myValidationSupport));
+        // registerInterceptor(new ResponseTerminologyDisplayPopulationInterceptor(myValidationSupport));
     }
 }
