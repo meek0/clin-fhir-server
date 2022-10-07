343d42d82573a9c1d12be0be136fb5bdea069af7
Yann MARCOU <creativeyann17@gmail.com>
2022-10-07 13:42:34 -0400

    fix: CLIN-1455 - multiple persons robustness

--- src/main/java/bio/ferlab/clin/interceptors/metatag/PrescriptionMasking.java
+++ src/main/java/bio/ferlab/clin/interceptors/metatag/PrescriptionMasking.java
@@ -24,9 +24,11 @@ public class PrescriptionMasking {
   public void preShow(MetaTagResourceAccess metaTagResourceAccess, Person personToShow, RequestDetails requestDetails) {
     final List<IBaseResource> resources = sameRequestInterceptor.get(requestDetails);
     if (personToShow != null && MaskingUtils.isValidPrescriptionRequest(resources)) {
+      // the query may contain several Person, find the good one
       // check if personToShow and personFromRequest are the same (because not same JAVA object)
-      final Person personFromRequest = FhirUtils.extractAllOfType(resources, Person.class).get(0);
-      if (FhirUtils.equals(personFromRequest, personToShow) && !MaskingUtils.isAlreadyMasked(personToShow)) {
+      final Person personFromRequest = FhirUtils.extractAllOfType(resources, Person.class).stream()
+          .filter(p -> FhirUtils.equals(p, personToShow)).findFirst().orElse(null);
+      if (personFromRequest != null && !MaskingUtils.isAlreadyMasked(personToShow)) {
         final List<String> userTags = metaTagResourceAccess.getUserTags(requestDetails);
         maskSensitiveData(metaTagResourceAccess, userTags, MaskingUtils.extractAnalysis(resources), personToShow);
       }
