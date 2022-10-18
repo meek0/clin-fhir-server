6eb2a98673d2cf075c071caf8a5c08c9a6e67887
Yann MARCOU <creativeyann17@gmail.com>
2022-10-18 17:10:34 -0400

    feat: CLIN-1495 - allow '.' in names

--- src/main/java/bio/ferlab/clin/validation/utils/ValidationPatterns.java
+++ src/main/java/bio/ferlab/clin/validation/utils/ValidationPatterns.java
@@ -2,6 +2,6 @@ package bio.ferlab.clin.validation.utils;
 
 import java.util.regex.Pattern;
 public class ValidationPatterns {
-    public static final Pattern NO_SPECIAL_CHARACTERS = Pattern.compile("^[a-zA-Z0-9- '\\u00C0-\\u00FF]*$");
+    public static final Pattern NO_SPECIAL_CHARACTERS = Pattern.compile("^[a-zA-Z0-9- .'\\u00C0-\\u00FF]*$");
     public static final Pattern VALID_RAMQ = Pattern.compile("^[A-Z]{4}\\d{8,9}$");
 }
