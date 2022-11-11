d633f46c0ee1b1909f05c48292a4652076289a3d
Yann MARCOU <creativeyann17@gmail.com>
2022-11-11 13:01:04 -0500

    feat: CLIN-1554 - add TUs

---
+++ src/test/java/bio/ferlab/clin/es/MigrationManagerTest.java
@@ -0,0 +1,75 @@
+package bio.ferlab.clin.es;
+
+import bio.ferlab.clin.es.config.ResourceDaoConfiguration;
+import bio.ferlab.clin.es.indexer.NanuqIndexer;
+import bio.ferlab.clin.properties.BioProperties;
+import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
+import ca.uhn.fhir.rest.api.server.IBundleProvider;
+import ca.uhn.fhir.rest.server.SimpleBundleProvider;
+import org.hl7.fhir.r4.model.ServiceRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static bio.ferlab.clin.es.TemplateIndexer.ANALYSES_TEMPLATE;
+import static bio.ferlab.clin.es.TemplateIndexer.SEQUENCINGS_TEMPLATE;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class MigrationManagerTest {
+
+  private final TemplateIndexer templateIndexer = Mockito.mock(TemplateIndexer.class);
+  private final BioProperties bioProperties = Mockito.mock(BioProperties.class);
+  private final ElasticsearchRestClient esClient = Mockito.mock(ElasticsearchRestClient.class);
+  private final NanuqIndexer nanuqIndexer = Mockito.mock(NanuqIndexer.class);
+  final IFhirResourceDao<ServiceRequest> serviceRequestDao = Mockito.mock(IFhirResourceDao.class);
+  final ResourceDaoConfiguration daoConfiguration = new ResourceDaoConfiguration(null, null, serviceRequestDao, null, null
+    , null, null, null, null, null, null);
+
+  private final MigrationManager migrationManager = new MigrationManager(templateIndexer, bioProperties, esClient, nanuqIndexer, daoConfiguration);
+
+  @BeforeEach
+  void beforeEach() {
+    when(bioProperties.getNanuqEsAnalysesIndex()).thenReturn("analyses");
+    when(bioProperties.getNanuqEsSequencingsIndex()).thenReturn("sequencings");
+  }
+
+  @Test
+  void no_migration() {
+
+    when(templateIndexer.indexTemplates()).thenReturn(Map.of(ANALYSES_TEMPLATE, "HASH1", SEQUENCINGS_TEMPLATE, "HASH1"));
+    when(esClient.aliases()).thenReturn(Map.of("analyses", "analyses-HASH1", "sequencings", "sequencings-HASH1"));
+
+    migrationManager.startMigration();
+
+    verify(nanuqIndexer, never()).doIndex(eq(null), any(), any(), any(), eq(false));
+  }
+
+  @Test
+  void migration() {
+
+    when(templateIndexer.indexTemplates()).thenReturn(Map.of(ANALYSES_TEMPLATE, "HASH2", SEQUENCINGS_TEMPLATE, "HASH1"));
+    when(esClient.aliases()).thenReturn(Map.of("analyses", "analyses-HASH1", "sequencings", "sequencings-HASH1"));
+
+    ServiceRequest sr1 = new ServiceRequest();
+    sr1.setId("sr1");
+    ServiceRequest sr2 = new ServiceRequest();
+    sr2.setId("sr2");
+    final SimpleBundleProvider bundleProvider1 = new SimpleBundleProvider(List.of(sr1, sr2));
+    final SimpleBundleProvider bundleProvider2 = new SimpleBundleProvider();
+    when(serviceRequestDao.search(any())).thenReturn(bundleProvider1).thenReturn(bundleProvider2);
+
+    migrationManager.startMigration();
+
+    verify(nanuqIndexer).doIndex(eq(null), eq(Set.of("sr1", "sr2")), eq("analyses-HASH2"), eq("sequencings-HASH1"), eq(false));
+    verify(esClient).setAlias(eq(List.of("analyses-HASH2")), eq(List.of("analyses-HASH1")), eq("analyses"));
+    verify(esClient, never()).setAlias(any(), any(), eq("sequencings"));
+    verify(esClient).delete(eq(List.of("analyses", "sequencings")));
+    verify(esClient).delete(eq(List.of("analyses-HASH1")));
+  }
+}
